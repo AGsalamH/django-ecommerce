38cb1414783b5dd0b9591b1f0fd279b6b1708c02
AGsalamH <AGsalamH@gmail.com>
2021-12-09 11:27:07 +0200

    Alter User model: Add phone field

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,7 @@ UserModel = get_user_model()
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = UserModel
-        fields = ('email','first_name', 'last_name')
+        fields = ('email','first_name', 'last_name', 'phone')
 
 
 class UserLoginForm(forms.Form):
--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
+from django.core import validators
 from django.db import models
 from django.utils.translation import gettext_lazy as _  
 from django.contrib.auth.models import AbstractUser
-
+from django.core.validators import MinLengthValidator
 from .managers import CustomUserManager
 
 # Create your models here.
@@ -9,6 +10,7 @@ from .managers import CustomUserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
+    phone = models.CharField(max_length=30, blank=True, validators=[MinLengthValidator(11)])
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
